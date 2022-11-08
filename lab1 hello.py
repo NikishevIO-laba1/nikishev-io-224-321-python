9f6e880509ff7a2e5fc255a6cc0653c02034f864
Your Name <you@example.com>
2022-11-08 14:53:15 +0300

    дададада

---
+++ lab1 hello.py
@@ -0,0 +1,15 @@
+print("ПРИВЕТ:")
+name ="маша";
+print("привет ,"+name +' !')
+print(f"привет, {name}!")
+print("привет, ",name, '!')
+print(1,2,3,4)
+print(1,2,3,4, sep=', ')
+print(1,2,3,4, sep='\n')
+print(1,2,3,4, sep='_')
+def AAA():
+    a=1 
+    a=1 
+    b=2
+    print(a+b)
+AAA()
