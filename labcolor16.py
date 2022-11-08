6776041bb8a61c75206b98b992da026bd2148417
Your Name <you@example.com>
2022-11-08 18:34:11 +0300

    Лабораторные работы Никишев

--- labcolor16.py
+++ labcolor16.py
@@ -4,7 +4,7 @@ print('\x1b[6;30;40mПривет!\x1b[0m')
 for a in range(30,38):
     for b in range(40,48):
         c=str(a)+';'+str(b)
-        print('\x1b['
-        #+str(b-40)+
-        ';'+c+'m'+c+'\x1b[0m', end="")
+        print('\x1b['+
+        #str(b-40)+
+        '0;'+c+'m'+c+'\x1b[0m', end="")
     print()
