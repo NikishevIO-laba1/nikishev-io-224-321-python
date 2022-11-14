140937aad4fab3d7c45ab73216fd436f5b84736e
Your Name <you@example.com>
2022-11-14 13:24:50 +0300

    лабораторные Никишев

--- labcolor16.py
+++ labcolor16.py
@@ -4,7 +4,8 @@ print('\x1b[6;30;40mПривет!\x1b[0m')
 for a in range(30,38):
     for b in range(40,48):
         c=str(a)+';'+str(b)
-        print('\x1b['+
-        #str(b-40)+
-        '0;'+c+'m'+c+'\x1b[0m', end="")
+        print('\x1b['+c+'m'+c+'\x1b[0m', end="")
     print()
+
+print('#2')
+for a in range (0,64): print("\x1b[", 30+a//8, ';', 40+a%8,'m', 30+a//8 , ';', 40+a%8,"\x1b[0m" ,'\n'*((a%8)-7==0) ,sep='', end='')
--- labdrawrectangle.py
+++ labdrawrectangle.py
@@ -1,10 +1,28 @@
 #hw
-char = '█'
-w=int(input('ширина'))
-h=int(input('высота'))
-f=input('Закрасить? y/n: ')
+#w=int(input('ширина'))
+#h=int(input('высота'))
+#f=input('Закрасить? y/n: ')
+w=20
+h=10
+f='n'
 is_fill=f.lower()=='y'
-char_fill=char if is_fill else ' '
-for r in range(h):
-    print(char*w)
+char_fill='█' if is_fill else ' '
+#for r in range(h):
+    #print(char_fill*w)
     #hw сделать так чтобы была рамка
+
+print('#1')
+char_frame=' ' if is_fill else '█'
+for r in range(h):
+    if r==0: print(char_frame*w)
+    elif r==h-1: print(char_frame*w)
+    else: print(char_frame+(char_fill*(w-2)+char_frame)*(w>1))
+
+print('#2')
+print(char_frame*w+('\n'+(h-2)*(char_frame+(char_fill*(w-2)+char_frame)*(w>1)+'\n')+(char_frame*w))*(h>1))
+
+print('змея')
+for i in range(h):
+    if i%4==0 or i%4==2: print(char_frame*w)
+    elif i%4==1 :print(char_fill*(w-1)+char_frame)
+    else:print(char_frame+char_fill*(w-1))
